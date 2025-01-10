33c2333122ac2b2d2e5d29f6a17cc446efd4c174
sarthak kumar <sarthakkumar10151@gmail.com>
2025-01-11 00:55:01 +0530

    imporve templates

--- Templates/CP_Templates/template.cpp
+++ Templates/CP_Templates/template.cpp
@@ -1,31 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
-typedef pair<int, int> pii;
-typedef pair<long long, long long> pll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<vi> vvi;
-typedef vector<vll> vvll;
-typedef vector<pii> vpii;
-typedef vector<pll> vpll;
-
-#define pb push_back
-#define fi first
-#define se second
-#define forn(i, n) for(int i = 0; i < n; i++)
-#define ford(i, n) for(int i = n - 1; i >= 0; i--)
-#define for1(i, n) for(int i = 1; i <= n; i++)
-#define forr(i, l, r) for(int i = l; i <= r; i++)
-#define forrd(i, l, r) for(int i = r; i >= l; i--)
-#define all(a) (a).begin(), (a).end()
-#define rall(a) (a).rbegin(), (a).rend()
-#define len(a) (int)(a).size()
-#define sq(x) (x) * (x)
-
 #ifdef SARTHAK_LOCAL
 #include "/Users/sarthakkumar/work/Cpp/Templates/CP_Templates/debug.cpp"
 #else 
--- Templates/Number_Theory/RandomNumber.cpp
+++ Templates/Number_Theory/RandomNumber.cpp
@@ -8,19 +8,17 @@ class Random {
 typedef long long ll;
 public:
     Random(const mt19937_64& _rng): m_rng(_rng) {}
-
 public:
-    bool nextBoolean() { return nextInt(2); }
-    int nextInt() { return nextInt(INT32_MAX); }
-    int nextInt(int x) { return (int)nextLong(ll(x)); }
-    int nextInt(int a, int b) { return (int)nextLong(ll(a), ll(b)); }
-    ll nextLong() { return m_rng(); }
-    ll nextLong(ll x) { return nextLong(0, x); }
-    ll nextLong(ll a, ll b) { return std::uniform_int_distribution<ll>(a, b - 1)(m_rng); }
-    vector<int> nextVector(int n, int mx = 1e9) { vector<int> a(n); for (int& x : a) x = nextInt(mx + 1); return a; }
-    vector<ll> nextVectorll(int n, ll mx = 1e18) { vector<ll> a(n); for (ll& x : a) x = nextLong(mx + 1); return a; }
-    
+    bool next_bool() { return next_int(2); }
+    int next_int() { return next_int(INT32_MAX); }
+    int next_int(int x) { return (int)next_long(ll(x)); }
+    int next_int(int a, int b) { return (int)next_long(ll(a), ll(b)); }
+    ll next_long() { return m_rng(); }
+    ll next_long(ll x) { return next_long(0, x); }
+    ll next_long(ll a, ll b) { return std::uniform_int_distribution<ll>(a, b - 1)(m_rng); }
+    vector<int> next_vector(int n, int mx = 1e9) { vector<int> a(n); for (int& x : a) x = next_int(mx + 1); return a; }
+    vector<ll> next_vectorll(int n, ll mx = 1e18) { vector<ll> a(n); for (ll& x : a) x = next_long(mx + 1); return a; }
 private:
     mt19937_64 m_rng;
 };
-Random random(rng);
+Random Rng(rng);
