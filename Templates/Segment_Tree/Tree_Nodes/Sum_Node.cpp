d2a77054bd5b08e915a916659f68984481efad65
sarthak kumar <sarthakkumar10151@gmail.com>
2025-01-11 04:17:30 +0530

    refactor

--- Templates/Segment_Tree/Tree_Nodes/Sum_Node.cpp
+++ Templates/Segment_Tree/Tree_Nodes/Sum_Node.cpp
@@ -7,17 +7,14 @@ public:
     SumNode() {}
     template<typename U> SumNode(U v) : sum(v) {}
 public:
-    static SumNode merge(const SumNode& a, const SumNode& b);
+    static SumNode merge(const SumNode& a, const SumNode& b) {
+        SumNode res;
+        res.sum = a.sum + b.sum;
+        return res;
+    }
 public:
     T sum = 0;
 };
+template<typename T> using Segtree = SegtreeImpl<SumNode<T>>;
 
 string to_string(const SumNode<int>& node) { return "{ " + to_string(node.sum) + " }"; }
-
-template<typename T> SumNode<T> SumNode<T>::merge(const SumNode<T>& a, const SumNode<T>& b) {
-    SumNode res;
-    res.sum = a.sum + b.sum;
-    return res;
-} 
-
-template<typename T> using Segtree = SegtreeImpl<SumNode<T>>;
